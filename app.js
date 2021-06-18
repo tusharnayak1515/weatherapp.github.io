fa3b4e6314807a4e2b176d2e47880df8b4badd3e
Tushar Ranjan Nayak <tusharnayak1515@gmail.com>
2021-06-19 00:27:41 +0530

    First

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const http = require("http");
+require('dotenv').config()
+
+const app = express();
+
+app.use("/static", express.static(path.join(__dirname, "/static")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.post("/", (req, res) => {
+    const query = req.body.cityName;
+    const apiKey = process.env.api_Key;
+    const url = "http://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=metric";
+
+    http.get(url, function (response) {
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            
+            res.render("weather", { city: query, temperature: temp, weather: weatherDescription, imageUrl: imageUrl });
+        });
+    });
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server started successfully at port 3000.");
+});
